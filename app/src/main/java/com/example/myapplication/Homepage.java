1e440ab74392d45428ede20df35a8d829a169097
unknown <dewiayupurnama333@gmail.com>
2021-03-25 21:15:02 +0700

    add all file

---
+++ app/src/main/java/com/example/myapplication/Homepage.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Homepage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homepage);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
+    private Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        username = findViewById(R.id.login_username);
+        password = findViewById(R.id.login_password);
+        login = findViewById(R.id.login_button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uname = username.getText().toString();
+                String pass = password.getText().toString();
+               if(uname.equals("dewik")&& pass.equals("dewik123")){
+                    startActivity(new Intent(MainActivity.this, Homepage.class));
+               }
+            }
+        });
+    }
+}
